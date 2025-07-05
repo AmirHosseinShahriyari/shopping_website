d0271b909f62dc8646b56cda2efd2bc29acf1310
Erfan-Hamedi <erfanhanew@gmail.com>
2025-07-05 20:43:56 +0330

    change fav_icon.php

--- user/dashbord.php
+++ user/dashbord.php
@@ -7,9 +7,17 @@ include "../includes/fav_icon.php";
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/user/style_dashbord.css">
     <title>Dashbord</title>
 </head>
 <body>
-    
+    <div class="quick_access-box">
+        <div class="edit_account_box"></div>
+        <div class="support_box"></div>
+        <div class="edit_account_box"></div>
+        
+    </div>
+
+
 </body>
 </html>
