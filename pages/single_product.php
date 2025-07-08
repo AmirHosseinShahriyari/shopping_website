70c25594c2dc3c3ca06ae5676e3ccfb63cd6965d
Erfan-Hamedi <erfanhanew@gmail.com>
2025-07-08 13:08:14 +0330

    add single_product

---
+++ pages/single_product.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/fonts.css">
+    <link rel="stylesheet" href="../css/reset.css">
+    <link rel="stylesheet" href="../css/fonts/font awesome.css">
+    <link rel="stylesheet" href="../css/page/style_single_product.css">
+    <title>single_product</title>
+</head>
+<body>
+    <div class="single_product_container">
+
+    <div class="product_img">
+
+    <img src="../assets/image & icon/product_img.webp" alt="عکس محصول">
+    
+    </div>
+
+    <div class="title_product_container">
+
+    <h2 class="title_product"> تاتل محصول</h2>
+
+    </div>
+
+    <div class="add_to_cart_container">
+        <h2></h2>
+    </div>
+
+    </div>
+</body>
+</html>
